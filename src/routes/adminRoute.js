398d72365e3d6dc24f89b50b5030e0aa8911f7a2
SrPatsu <paulosergiopierdona@gmail.com>
2024-11-21 22:48:38 -0300

    admin templante

--- src/routes/adminRoute.js
+++ src/routes/adminRoute.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const { isAuthenticated } = require("./authentication")
+const { isAdmin } = require("./authentication")
+
 
 //* get admin page html
-router.get('/register_product', isAuthenticated, (req, res) =>{
+router.get('/', isAdmin, (req, res) =>{
+  const title = 'Admin main';
+  res.render('./admin/layout_adm', {title},);
+});
+
+
+router.get('/register_product', isAdmin, (req, res) =>{
     const title = 'Register Product';
     const post_destiny = '/products/';
     res.render('./admin/create_product', {title, post_destiny},);
